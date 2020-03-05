548e95ae0b1afac3282f9c1899e881b595b11195
Yarema Zaiachuk <jarema2505@gmail.com>
2020-03-05 17:23:47 +0200

    1st commit

---
+++ Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,63 @@
+//
+//  FlightInterfaceController.swift
+//  Watch Extension
+//
+//  Created by Yarema Zaiachuk on 04.03.2020.
+//  Copyright © 2020 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FlightInterfaceController: WKInterfaceController {
+  
+  @IBOutlet var flightLabel: WKInterfaceLabel!
+  @IBOutlet var routeLabel: WKInterfaceLabel!
+  @IBOutlet var boardingLabel: WKInterfaceLabel!
+  @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+  @IBOutlet var statusLabel: WKInterfaceLabel!
+  @IBOutlet var gateLabel: WKInterfaceLabel!
+  @IBOutlet var seatLabel: WKInterfaceLabel!
+  
+  // 1
+  var flight: Flight? {
+    // 2
+    didSet {
+      // 3
+      guard let flight = flight else { return }
+      // 4
+      flightLabel.setText("Flight \(flight.shortNumber)")
+      routeLabel.setText(flight.route)
+      boardingLabel.setText("\(flight.number) Boards")
+      boardTimeLabel.setText(flight.boardsAt)
+      // 5
+      if flight.onSchedule {
+        statusLabel.setText("On Time")
+      } else {
+        statusLabel.setText("Delayed")
+        statusLabel.setTextColor(.red)
+      }
+      gateLabel.setText("Gate \(flight.gate)")
+      seatLabel.setText("Seat \(flight.seat)")
+    }
+  }
+  
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+    
+    if let flight = context as? Flight {
+      self.flight = flight
+    }    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+}
---
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -0,0 +1,34 @@
+//
+//  ScheduleInterfaceController.swift
+//  Watch Extension
+//
+//  Created by Yarema Zaiachuk on 05.03.2020.
+//  Copyright © 2020 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ScheduleInterfaceController: WKInterfaceController {
+  
+  @IBOutlet weak var flightsTable: WKInterfaceTable!
+  
+  var flights = Flight.allFlights()
+  
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+    flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
+    for index in 0..<flightsTable.numberOfRows {
+      guard let controller = flightsTable.rowController(at: index) as? FlightRowController else { continue }
+
+      controller.flight = flights[index]
+    }
+  }
+  
+  override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+    let flight = flights[rowIndex]
+    presentController(withName: "Flight", context: flight)
+  }
+  
+}
